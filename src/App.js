050b1ba980800c086088c966a9fed0a695498141
Nehal <mdnehaladil@gmail.com>
2023-03-06 11:07:46 +0530

    tooltip component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
-import Tooltip from "./Tooltip";
+import "./index.css";
+import ToolTip from "./Tooltip";
 
 function App() {
-  const [position, setPosition] = useState("top");
-
-  const handleClick = () => {
-    if (position === "top") {
-      console.log("position", position);
-    }
-  };
-
   return (
     <div className="App">
       <div>
-        <Tooltip title="top" handleClick={handleClick} />
+        <ToolTip position="top" />
       </div>
     </div>
   );
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,14 +1,45 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import "./Tooltip.css";
 
-export default function Tooltip(props) {
-  console.log(props);
+const ToolTip = ({ position }) => {
+  const spanRef = useRef();
+
+  useEffect(() => {
+    //top tooltip
+    if (position === "top") {
+      spanRef.current.style.bottom = "120%";
+      spanRef.current.style.left = "50%";
+      spanRef.current.style.marginLeft = "-110px";
+    }
+
+    //bottom tooltip
+    if (position === "bottom") {
+      spanRef.current.style.top = "140%";
+      spanRef.current.style.left = "50%";
+      spanRef.current.style.marginLeft = "-110px";
+    }
+
+    //left tooltip
+    if (position === "left") {
+      spanRef.current.style.top = "-50%";
+      spanRef.current.style.right = "110%";
+    }
+
+    //right tooltip
+    if (position === "right") {
+      spanRef.current.style.top = "-50%";
+      spanRef.current.style.left = "110%";
+    }
+  }, [position]);
 
   return (
-    <div className="container">
-      <button className="btn" onClick={props.handleClick}>
-        Hover Over Me!
-      </button>
-      <h1>{props.title}</h1>
+    <div className="tooltip">
+      Hover over me!
+      <span ref={spanRef} className="tooltiptext">
+        I'm a Simple tooltip
+      </span>
     </div>
   );
-}
+};
+
+export default ToolTip;
